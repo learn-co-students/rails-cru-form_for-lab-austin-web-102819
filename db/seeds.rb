ea7d5c2fdef6317307ae6f52aeee0ce7c4abb574
BrittneyHaselmayer <brittneyhaselmayer@yahoo.com>
2019-11-30 18:50:10 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+a = Artist.create(name: "A" , bio: "A-team")
+b = Artist.create(name:"B", bio: "Bees knees")
+c = Artist.create(name:"C", bio: "Ceeya")
+
+
+rap = Genre.create(name: "Rap")
+country = Genre.create(name: "Country")
+classic = Genre.create(name: "Classical")
+
+
+one = Song.create(name: "Uno" , artist_id: a.id , genre_id: rap.id) 
+two = Song.create(name: "Dos", artist_id: b.id , genre_id: country.id)
+three = Song.create(name: "Tres", artist_id: c.id, genre_id: classic.id)
